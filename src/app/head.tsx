c98921255a735d032e95eb32fb04eabfbdc52ba4
leonardo sousa <leonardocasa790@gmail.com>
2023-02-07 17:25:08 -0300

    add meta-tag

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Encontre gradientes vibrante e atraentes para melhorar a aparência da sua interface de usuário com facilidade. Nossa ampla gama de opções fornece soluções para designers de UI e programadores. Experimente agora!" />
       <link rel="icon" href="/favicon.png" />
+      <meta name="google-site-verification" content="XSCEQzoKGaB-2-wXcBxlLmKBiN3tUIVavml4mLGQEgw" />
     </>
   )
 }
