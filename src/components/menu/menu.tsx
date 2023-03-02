08f191e32266958ae9fda5718ca1ababc1133b94
leonardo sousa <leonardocasa790@gmail.com>
2023-03-02 17:03:35 -0300

    update links

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from './styles.module.css'
 
+
 export default function Menu(){
     return(
         <nav className={styles.menu}>
@@ -10,9 +11,11 @@ export default function Menu(){
                 <Image src={logo} alt="logo gradienthub" className={styles.img}/>
             </div>
             <ul className={styles.list}>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/">Gradients</Link></li>
-                <li><Link href="/">Why use 🤔</Link></li>
+                <li>
+                    <Link href="#"className={styles.link}>Home</Link>
+                </li>
+                <li><Link href="/" className={styles.link}>Gradients</Link></li>
+                <li><Link href="#SectionWhy"className={styles.link}>Why use 🤔</Link></li>
             </ul>
         </nav>
     )
--- src/components/sectionWhy/sectionwhy.tsx
+++ src/components/sectionWhy/sectionwhy.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 export default function SectionWhy(){
     return(
-        <section>
+        <section id="test">
             <h2>🤔 why use ? </h2>
             <p className={styles.paragraph}>Gradients are one of the most effective tools for improving the aesthetics of your design.Gradients are one of the most effective tools for improving the aesthetics of your design. Through the use of smooth color transitions, gradients can help create depth and dimension in a design.</p>
             <p className={styles.paragraph}>Here on our website, we offer a wide selection of ready-to-use gradients.</p>
